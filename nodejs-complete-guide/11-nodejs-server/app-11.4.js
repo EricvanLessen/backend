d189c70271d37b3ee4ac27fed0f023fb084a9781
EricvanLessen <eric.vanlessen@googlemail.com>
2023-08-12 21:38:47 +0200

    nodejs

--- nodejs-complete-guide/11-nodejs-server/app-11.4.js
+++ nodejs-complete-guide/11-nodejs-server/app-11.4.js
@@ -4,8 +4,10 @@ const http = require('http');
 // callback function is called 
 // when a request reaches the server
 const server = http.createServer((req, res) => {
-    // we log the request
-    console.log(req);
+    // output request attributes
+    console.log(req.url);
+    console.log(req.method);
+    console.log(req.header);
 });
 
 server.listen(3000);
